655a3a90ee47dae2bdaabb214ad7e5642241295d
minodora-c <minodora.cristescu@campus.tu-berlin.de>
2022-06-29 12:19:42 +0200

    until Vid 83

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,41 +8,14 @@ import Info from "./components/Info";
 function App(props) {
 
   const dyn = "Yeeees";
-  let ort = "hier";
-
-
-  async function changeName2() {
-    const callApi = await fetch("https://randomuser.me/api/");
-    const {results} = await callApi.json();
-    this.name = results[0].name.first;
-  }
-
-  function changeName() {
-    ort = "Other Person"
-
-
-  }
 
   return (
     <div className="App">
       <header className="App-header">
         <Info />
-        <p>Test von {ort}</p>
         <p>Test aus {props.nameFromProps}</p>
         <p>Dynamic content?&nbsp;&nbsp;&nbsp; {dyn}</p>
-        <button onClick={changeName.bind(this)}>Change Name</button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- my-app/src/components/Info.js
+++ my-app/src/components/Info.js
@@ -1,11 +1,60 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import './Info.css';
 
 class Info extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            ort: "Test",
+            name: "John Doe",
+            count: 0,
+            elements: []
+        }
+    };
+
+    async changeName3() {
+        const callApi = await fetch("https://randomuser.me/api/");
+        const {results} = await callApi.json();
+
+        this.setState({
+            ort: "Other Person",
+            name: results[0].name.first,
+            count: this.state.count + 1,
+            elements: this.state.elements.concat({
+                num: this.state.count + 1,
+                oldName: results[0].name.first,
+                gender: results[0].gender
+            })
+        })
+
+    }
+
     render() {
+        let updateName = ""
+        if (this.state.name != "John Doe") {
+            updateName = <div><p>Name updated <b>{this.state.count}x</b> :)</p>
+            </div>
+        }
+
+        let list = this.state.elements.map(
+            (el) => {
+                return <li key={el.num}
+                           style={{'background-color': el.gender == 'male' ? 'lightblue' : 'pink'}}>
+                            {el.num} {el.oldName}
+                        </li>
+            }
+        )
         return (
-            <div>
+            <div id={"divInfo"}>
                 <h1>The Info</h1>
-                <p>Reacttt test</p>
+                <p>Test aus Info: {this.state.ort}</p>
+                <p>Name aus RandomUser: {this.state.name}</p>
+                {updateName}
+                <button onClick={this.changeName3.bind(this)}>Get Name</button>
+                <ul>
+                    {list}
+                </ul>
             </div>
         )
     }
